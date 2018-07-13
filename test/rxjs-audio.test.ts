9c07af05b661b6f34d17d35717d5b11a329d9b5f
Indermohan Singh <indermohansinghk7@gmail.com>
2018-07-13 15:04:48 +0200

    test: add first test for library

---
+++ test/rxjs-audio.test.ts
@@ -0,0 +1,14 @@
+import { RxJSAudioService } from '../src/rxjs-audio'
+
+/**
+ * RxJSAudioService test
+ */
+describe('RxJSAudioService test', () => {
+  it('works if true is truthy', () => {
+    expect(true).toBeTruthy()
+  })
+
+  it('RxJSAudioService is instantiable', () => {
+    expect(new RxJSAudioService()).toBeInstanceOf(RxJSAudioService)
+  })
+})
