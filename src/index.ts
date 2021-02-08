c3f60f147361b328f2b38747753d8ff5c2e9d3a9
Jonas Snellinckx <jonas.snellinckx@gmail.com>
2021-02-08 16:41:45 +0100

    feat: add more audo functionality + split up

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
+// import "core-js/fn/array.find"
+// ...
+
+// barrel
+export * from './rxjs-audio-stream';
+export * from './rxjs-playlist-audio-stream';
+export * from './rxjs-audio.interfaces';
